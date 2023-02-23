9c54f792f23eacf0f19d0ff3b648cefe1f99d5cf
jprasad09 <phjadhav09@gmail.com>
2023-02-23 18:51:49 +0530

    cors issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://ivory-yak-tux.cyclic.app/api',
+  baseURL: 'https://ratemyfoodserver.onrender.com/api',
   withCredentials: true
 });
 
